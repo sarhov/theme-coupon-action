7f837d9607e22139bfdd7a2ee0d59874906c9879
Սարգսյան Հովհաննես <sarhov13@yahoo.com>
2013-09-01 14:19:26 +0400

    update - sidebar items -finish

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,12 @@
 jQuery(document).ready(function($) {
-	
+	$('.msnim-opener-wrap').click(function() {
+		$('.msni-more').slideToggle('slow');
+		$('.msnim-opener').toggleClass('active');
+		if($('.msnim-opener').hasClass('active')){
+			$('.msnim-opener').html('less categories');
+		}
+		else{
+			$('.msnim-opener').html('more categoires');
+		}
+	});
 });
