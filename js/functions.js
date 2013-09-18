c6bdaaa00d996554234d30c8f7053b0d9a1b0985
Սարգսյան Հովհաննես <sarhov13@yahoo.com>
2013-09-18 14:43:38 +0400

    scroll-to-top in process

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,32 @@
 jQuery(document).ready(function($) {
-	$('.msnim-opener-wrap').click(function() {
-		$('.msni-more').slideToggle('slow');
-		$('.msnim-opener').toggleClass('active');
-		if($('.msnim-opener').hasClass('active')){
-			$('.msnim-opener').html('less categories');
-		}
-		else{
-			$('.msnim-opener').html('more categoires');
-		}
+	$('.msnim-opener-wrap').click(function () {
+	    $('.msni-more').slideToggle('slow');
+	    $('.msnim-opener').toggleClass('active');
+	    if ($('.msnim-opener').hasClass('active')) {
+	        $('.msnim-opener').html('less categories');
+	    } else {
+	        $('.msnim-opener').html('more categoires');
+	    }
 	});
+
+
+	 // The scrolling to top
+	$(window).scroll(function () {
+	    if ($(this).scrollTop() > 200) {
+	        $('.scroll-to-top').fadeIn();
+	    } else {
+	        $('.scroll-to-top').fadeOut();
+	    }
+	});
+	$('.scroll-to-top').click(function () {
+	    $('body,html').animate({
+	        scrollTop: 0
+	    }, 800);
+	    return false;
+	});
+
+
+
+// don't delete, the ready END
+
 });
